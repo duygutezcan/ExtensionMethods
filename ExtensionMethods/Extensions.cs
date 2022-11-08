bed105acfae06854a801813ec99498289210638c
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-08 09:23:00 +0300

    Add project files.

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static string IlkHarfBuyuk(this string value)     //string e ek özellik kazandırır
+        {
+            return value.Substring(0,1).ToUpper() + value.Substring(1);
+        }
+
+        public static  string  Ayadi( this DateTime date)
+        {
+            
+            int ay = date.Month;
+            if(ay==1)
+            {
+                return "ocak"; 
+            }
+            else if(ay==2)
+            {
+                return "şubat";
+            }
+            else if (ay == 3)
+            {
+                return "mart";
+            }
+            else if (ay == 4)
+            {
+                return "nisan";
+            }
+            else if (ay == 5)
+            {
+                return "mayıs";
+            }
+            else if (ay == 6)
+            {
+                return "haziran";
+            }
+            else if (ay == 7)
+            {
+                return "temmuz";
+            }
+            else if (ay == 8)
+            {
+                return "ağustos";
+            }
+            else if (ay == 9)
+            {
+                return "eylül";
+            }
+            else if (ay == 10)
+            {
+                return "ekim";
+            }
+            else if (ay == 11)
+            {
+                return "kasım";
+            }
+            else if (ay == 12)
+            {
+                return "aralık";
+            }
+            else 
+            {
+            return "ay yok";
+             }
+        }
+
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethods;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string isim = "duygu";
+        Console.WriteLine(isim.IlkHarfBuyuk());
+
+        //2.yol
+        //Console.WriteLine(Extensions.IlkHarfBuyuk("duygu"));
+
+       DateTime D = DateTime.Now;
+        Console.WriteLine(D.Ayadi());
+
+    }
+}
